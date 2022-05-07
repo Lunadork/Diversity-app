199a6ea28dd52bb9cb01e8c74fe92e6d18f9dfde
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-05-07 21:08:05 +0100

    created components for get started page and signup page , installed react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
+import React from 'react'
+import { Routes, Route } from  'react-router-dom';
+import * as Pages from './pages';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Diversity</h1>
-        <h3>Mental Health solutions</h3>
-      </header>
-    </div>
+
+    <Routes>
+       <Route index element={<Pages.Home />} />
+
+    </Routes>
+
+
+
   );
 }
-
 export default App;
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { GetStarted } from '../../components';
+
+
+
+export const Home = () => {
+  return (
+    <>
+    <GetStarted />
+    </>
+
+  )
+}
