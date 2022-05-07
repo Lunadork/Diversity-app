c1d6227d8e2471c3b825725c9a2a250b7946eea2
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-05-07 20:23:55 +0100

    added react build and netlify.toml

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome to Diversity</h1>
+        <h3>Mental Health solutions</h3>
       </header>
     </div>
   );
