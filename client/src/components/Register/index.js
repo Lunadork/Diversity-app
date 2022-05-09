9271d0a9ab924511df4fb5f04f13bacad4164272
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-05-09 01:42:14 +0100

    added register component for signup

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -3,8 +3,30 @@ import './style.css'
 
  export const Register= () => {
   return (
-    <div>
-      <h1> page for registration</h1>
+    <div className="form-content">
+      <form  className="form">
+        <h1>Register with mental health solutions</h1>
+        <div className="form-inputs">
+
+          <label htmlFor="username" className="form-label">User name</label>
+          <input id="username" type="text" name="username" className="form-input" placeholder="enter username"/>
+
+          <label htmlFor="username" className="form-label">email</label>
+          <input id="email" type="email" name="email" className="form-input" placeholder="enter email"/>
+
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" name="password" className="form-input" placeholder="enter password"/>
+
+          <label htmlFor="password2" className="form-label">Confirm password</label>
+          <input type="password2" name="password2" className="form-input" placeholder="confirm password"/>
+
+        </div>
+
+        <button className="form-input-bnt" type='submit'>Register</button>
+        <span className="form-input-login">already have an account? Login <a href="#">here</a></span>
+      </form>
+
+
     </div>
   )
 }
