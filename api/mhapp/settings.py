44ad9289324a1020f37840c594bf3ba245175cbc
Henry <uce.henrys@gmail.com>
2022-05-09 15:31:57 +0100

    deployed with migration of users

--- api/mhapp/settings.py
+++ api/mhapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-b22_@_e6y93^@#krebk*mitp%s5^s!jp-=-2di)=b+vh%v7-jq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
     'rest_framework',
 ]
 
@@ -83,7 +85,6 @@ DATABASES = {
 
 
 # Heroku: Update database configuration from $DATABASE_URL. 
-import dj_database_url 
 db_from_env = dj_database_url.config(conn_max_age=600) 
 DATABASES['default'].update(db_from_env)
 
