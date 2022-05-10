4855b844fa07ba7b43f1271b2dfdf0c9bc95f5fd
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-05-10 12:57:00 +0100

    moved client folder to api folder

--- client/src/components/index.js
+++
@@ -1,9 +0,0 @@
-
-export {GetStarted} from './GetStarted';
-export {Register} from './Register';
-export {RegisterSuccess} from './RegisterSuccess';
-
-export {default as Button} from './Button'
-export {  UseRegister } from './UseRegister'
-export {default as ValidateRegister} from './ValidateRegisterForm'
-export { Login } from './UseLogin'
--- client/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export {  Home } from './Home';
-export { Signup } from './Signup';
-export { LoginForm} from './LoginForm';
-export { GeneralAdvice } from './GeneralAdvice';
